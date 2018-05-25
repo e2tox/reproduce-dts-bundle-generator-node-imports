fa0c39a3281dd222ec4eb8d3b44f4bd614ff4411
e2tox <e2tox@live.com>
2018-05-25 09:28:54 +0800

    Server is not emitted in output

--- stoppable.d.ts
+++ stoppable.d.ts
@@ -2,6 +2,9 @@
 
 import { EventEmitter } from 'events';
 
+export { Server } from 'http';
+
 export declare class StoppableEventEmitter extends EventEmitter {
     emitErrorEvent(event: Error): boolean;
 }
+
